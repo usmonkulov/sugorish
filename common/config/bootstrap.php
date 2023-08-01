bdf7fcb9ac5bd7d24142c912feba7d361eda2031
Usmonkulov Bobur <58555131+usmonkulov@users.noreply.github.com>
2023-08-01 18:38:13 +0500

    01.08.2023 dagi o'zgarishlarim

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,4 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@restapi', dirname(dirname(__DIR__)) . '/restapi');
+Yii::setAlias('@settings', dirname(dirname(__DIR__)) . '/settings');
