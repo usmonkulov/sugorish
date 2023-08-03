d11a32fd7c6bca0687ab322b2b32511c4d72dd77
Usmonkulov Bobur <58555131+usmonkulov@users.noreply.github.com>
2023-08-03 18:18:00 +0500

    regstratsiya qismi tayyor bo'ldi

---
+++ console/migrations/m230803_055711_create_user_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user_roles}}`.
+ */
+class m230803_055711_create_user_roles_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->batchInsert('{{%auth_items}}', ['type', 'name', 'description'], [
+            [1, 'user', 'User'],
+            [1, 'admin', 'Admin'],
+        ]);
+
+        $this->batchInsert('{{%auth_item_children}}', ['parent', 'child'], [
+            ['admin', 'user'],
+        ]);
+
+        $this->execute('INSERT INTO {{%auth_assignments}} (item_name, user_id) SELECT \'user\', u.id FROM {{%user}} u ORDER BY u.id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('{{%auth_items}}', ['name' => ['user', 'admin']]);
+    }
+}
